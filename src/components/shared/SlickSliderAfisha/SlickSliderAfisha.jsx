e130478b627dacf5cb78efc6a7b6a1c6d2d47263
yvyletkov <yvyletkov@ya.ru>
2020-10-12 11:00:48 +0300

    slider edit

--- src/components/shared/SlickSliderAfisha/SlickSliderAfisha.jsx
+++ src/components/shared/SlickSliderAfisha/SlickSliderAfisha.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Slider from "react-slick";
 import CardSlider from "../CardSlider/CardSlider";
 import afisha1 from "../../../img/afisha/afisha1.png";
@@ -6,92 +6,6 @@ import afisha2 from "../../../img/afisha/afisha2.png";
 import afisha3 from "../../../img/afisha/afisha3.png";
 import afisha4 from "../../../img/afisha/afisha4.png";
 import afisha5 from "../../../img/afisha/afisha5.png";
-
-const imgMassive = [
-  {
-    img: afisha1,
-    title: "Караоке бар",
-    subtitle: "Green Stage",
-    time: "21:00 8 августа",
-    house: "Корпус Classic",
-    key: 1,
-  },
-  {
-    img: afisha2,
-    title: "Караоке бар",
-    subtitle: "Green Stage",
-    time: "21:00 8 августа",
-    house: "Корпус Classic",
-    key: 2,
-  },
-  {
-    img: afisha3,
-    title: "Караоке бар",
-    subtitle: "Green Stage",
-    time: "21:00 8 августа",
-    house: "Корпус Classic",
-    key: 3,
-  },
-  {
-    img: afisha4,
-    title: "Караоке бар",
-    subtitle: "Green Stage",
-    time: "21:00 8 августа",
-    house: "Корпус Classic",
-    key: 4,
-  },
-  {
-    img: afisha5,
-    title: "Караоке бар",
-    subtitle: "Green Stage",
-    time: "21:00 8 августа",
-    house: "Корпус Classic",
-    key: 5,
-  },
-  {
-    img: afisha5,
-    title: "Караоке бар",
-    subtitle: "Green Stage",
-    time: "21:00 8 августа",
-    house: "Корпус Classic",
-    key: 6,
-  },
-];
-
-const slider = imgMassive.map((e) => {
-  const { img, title, subtitle, time, house, key } = e;
-  return (
-    <div className="SliderElement" key={key}>
-      <CardSlider
-        img={img}
-        title={title}
-        subtitle={subtitle}
-        time={time}
-        house={house}
-      />
-    </div>
-  );
-});
-
-export default class SlickSliderAfisha extends Component {
-  render() {
-    const settings = {
-      className: "center",
-      centerMode: true,
-      infinite: true,
-      centerPadding: "60px",
-      slidesToShow: 5,
-      speed: 500,
-    };
-    return (
-      <div className="container">
-        <style>{cssstyle}</style>
-        <h2>Сегодня в программе</h2>
-        <Slider {...settings}>{slider}</Slider>
-      </div>
-    );
-  }
-}
 
 const cssstyle = `
 h3 {
@@ -116,3 +30,93 @@ h3 {
     transition: all .3s ease;
 }
 `;
+
+const imgMassive = [
+    {
+        img: afisha1,
+        title: "Караоке бар",
+        subtitle: "Green Stage",
+        time: "21:00 8 августа",
+        house: "Корпус Classic",
+        key: 1,
+    },
+    {
+        img: afisha2,
+        title: "Караоке бар",
+        subtitle: "Green Stage",
+        time: "21:00 8 августа",
+        house: "Корпус Classic",
+        key: 2,
+    },
+    {
+        img: afisha3,
+        title: "Караоке бар",
+        subtitle: "Green Stage",
+        time: "21:00 8 августа",
+        house: "Корпус Classic",
+        key: 3,
+    },
+    {
+        img: afisha4,
+        title: "Караоке бар",
+        subtitle: "Green Stage",
+        time: "21:00 8 августа",
+        house: "Корпус Classic",
+        key: 4,
+    },
+    {
+        img: afisha5,
+        title: "Караоке бар",
+        subtitle: "Green Stage",
+        time: "21:00 8 августа",
+        house: "Корпус Classic",
+        key: 5,
+    },
+    {
+        img: afisha5,
+        title: "Караоке бар",
+        subtitle: "Green Stage",
+        time: "21:00 8 августа",
+        house: "Корпус Classic",
+        key: 6,
+    },
+];
+
+
+const SlickSliderAfisha = (props) => {
+
+    const items = imgMassive.map((data) => {
+        const {img, title, subtitle, time, house, key} = data;
+        return (
+            <div className="SliderElement" key={key}>
+                <CardSlider
+                    img={img}
+                    title={title}
+                    subtitle={subtitle}
+                    time={time}
+                    house={house}
+                />
+            </div>
+        );
+    });
+
+    const settings = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "60px",
+        slidesToShow: 5,
+        speed: 500,
+    };
+
+    return (
+        <div className="container">
+            <style>{cssstyle}</style>
+            <h2>Сегодня в программе</h2>
+            <Slider {...settings}>{items}</Slider>
+        </div>
+    )
+};
+
+export default SlickSliderAfisha;
+
