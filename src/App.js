7411e694997d0fec9abeeb6fe8b76de2d4e02954
yvyletkov <yvyletkov@ya.ru>
2020-10-13 10:31:30 +0300

    intermediate commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.scss";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import "./style/normalize.css";
 import "./style/fonts.css";
 import SlickSliderAfisha from "./components/shared/SlickSliderAfisha/SlickSliderAfisha";
 import HomePage from "./components/HomePage/HomePage";
-import Headline from "./components/shared/Headline/Headline";
 import TextPlusImageBlock from "./components/shared/TextPlusImageBlock/TextPlusImageBlock";
 
 function App() {
@@ -16,6 +15,7 @@ function App() {
       <HomePage/>
       <SlickSliderAfisha></SlickSliderAfisha>
       {/*<Footer />*/}
+      <TextPlusImageBlock/>
     </>
   );
 }
