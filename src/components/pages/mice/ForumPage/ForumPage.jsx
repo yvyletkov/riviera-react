62978c6bb18bfcd47e56685acbc2bb1bf5ce3fec
f1tzy <winndini@gmail.com>
2020-11-17 15:42:41 +0300

    add block descr

--- src/components/pages/mice/ForumPage/ForumPage.jsx
+++ src/components/pages/mice/ForumPage/ForumPage.jsx
@@ -7,10 +7,15 @@ import {micePagesData, roomsAndPricesPageData} from "../../../../data";
 import WidthSpecialsSlider from "../../../shared/sliders/WidthSpecialsSlider/WidthSpecialsSlider";
 import RoomsSlider from "../../../shared/sliders/RoomsSlider/RoomsSlider";
 import s from "./ForumPage.module.scss";
-import kitchenImg1 from "../../../../img/events/wedding/kitchen1.jpg";
-import kitchenImg2 from "../../../../img/events/wedding/kitchen2.jpg";
+import imgAbout1 from '../../../../img/mice/conference/11.jpg'
+import imgAbout2 from '../../../../img/mice/conference/12.jpg'
 import Headline from "../../../shared/Headline/Headline";
 import Button from "../../../shared/Button/Button";
+import HeadlineCenter from "../../../shared/HeadlineCenter/HeadlineCenter";
+import partnerShip1 from '../../../../img/mice/conference/icons/10.png'
+import partnerShip2 from '../../../../img/mice/conference/icons/11.png'
+import partnerShip3 from '../../../../img/mice/conference/icons/12.png'
+import partnerShip4 from '../../../../img/mice/conference/icons/13.png'
 
 const ForumPage = () => {
     return (
@@ -26,7 +31,7 @@ const ForumPage = () => {
                 <CenteredSlider slides={micePagesData.centeredSlides} title={"Что мы берем на себя"} type={2} />
             </section>
             <section className="section">
-                <WidthSpecialsSlider slides={micePagesData.widthSpecialsSlider} />
+                <WidthSpecialsSlider slides={micePagesData.widthSpecialsSlider} title={"Наши залы"}/>
             </section>
             <section className='section'>
                 <RoomsSlider subtitle={'Корпус'} title={'Модерн'} textContent={roomsAndPricesPageData.modernDescr}
@@ -39,8 +44,8 @@ const ForumPage = () => {
                 <div className={s.forumAboutBlock}>
                     <div className={s.wrapper}>
                         <div className={s.container}>
-                            <img className={s.leftImg} src={kitchenImg1} alt=""/>
-                            <img className={s.rightImg} src={kitchenImg2} alt=""/>
+                            <img className={s.leftImg} src={imgAbout1} alt=""/>
+                            <img className={s.rightImg} src={imgAbout2} alt=""/>
                             <div className={s.textContent}>
                                 <Headline subtitle={'Ваша задача'} title={'Особенная?'}/>
                                 <p className={s.boldTitle}>Справимся и с ней!</p>
@@ -56,6 +61,15 @@ const ForumPage = () => {
                             </div>
                         </div>
                     </div>
+                </div>
+            </section>
+            <section className="section">
+                <HeadlineCenter title={"С кем мы сотрудничали"} />
+                <div className={s.flex}>
+                    <img className={s.partnerShip} src={partnerShip1} alt="Партнеры"/>
+                    <img className={s.partnerShip} src={partnerShip2} alt="Партнеры"/>
+                    <img className={s.partnerShip} src={partnerShip3} alt="Партнеры"/>
+                    <img className={s.partnerShip} src={partnerShip4} alt="Партнеры"/>
                 </div>
             </section>
         </>
