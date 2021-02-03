993082d466c8afdb82d5ff59a4af615dd3944f63
f1tzy <winndini@gmail.com>
2021-02-03 14:35:43 +0300

    fix conflicts

--- src/App.js
+++ src/App.js
@@ -59,7 +59,6 @@ const AnimationPage = lazy(() => import("./components/pages/AnimationPage/Animat
 const DefenderOfTheFatherlandDay = lazy(() => import("./components/pages/DefenderOfTheFatherlandDay/DefenderOfTheFatherlandDay"));
 const March8Page = lazy(() => import("./components/pages/March8Page/March8Page"));
 const TheGrilledPage = lazy(() => import("./components/pages/infrastructure/Restaurants/TheGrilledPage/TheGrilledPage"));
-const LanguageCampPage = lazy(() => import("./components/pages/LanguageCampPage/LanguageCampPage"));
 
 function App() {
 
@@ -85,9 +84,6 @@ function App() {
             </div>}>
 
                 <Switch>
-                    <Route path='/language-camp' exact
-                           component={() => <LanguageCampPage/>}/>
-
                     <Route path='/the-grilled' exact
                            component={() => <TheGrilledPage/>}/>
 
