f94abc042cb548faec258273b6f75cb787a15915
Yaroslav Vyletkov <yvyletkov@ya.ru>
2021-03-25 14:51:13 +0300

    update workshop page (not working forms now)

--- src/components/additional/WorkshopForm/WorkshopForm.jsx
+++ src/components/additional/WorkshopForm/WorkshopForm.jsx
@@ -36,35 +36,46 @@ const NumInput = ({
                     delimiters: [' (', ') ', '-', '-'],
                     blocks: [2, 3, 3, 2, 2],
                 }}
-            // onChange={this.onChange.bind(this)}
                 className={classNames}
                 {...field} {...props}
                 type={type}
         />
-        {/*<input type={type} className={classNames} {...field} {...props} />*/}
         {touched[field.name] &&
         errors[field.name] && <div className="error">{errors[field.name]}</div>}
     </div>
 };
 
-const Textarea = ({
-                   field,
-                   form: {touched, errors},
-                   ...props
-               }) => {
+const DateInput = ({
+                       field,
+                       type,
+                       form: {touched, errors},
+                       ...props
+                   }) => {
     const classNames = cx('input', {'success': touched[field.name] && !errors[field.name]}, {'error': touched[field.name] && errors[field.name]})
     return <div style={{position: 'relative'}}>
-        <textarea rows={6} className={classNames} {...field} {...props} />
+        <Cleave placeholder="Дата"
+                options={{
+                    delimiters: ['.', '.'],
+                    blocks: [2, 2, 4],
+                }}
+                className={classNames}
+                {...field} {...props}
+                type={type}
+        />
         {touched[field.name] &&
         errors[field.name] && <div className="error">{errors[field.name]}</div>}
     </div>
 };
 
-const WorkshopForm = ({type, }) => {
+
+const WorkshopForm = ({type,}) => {
+
+    const [showExtraField, setShowExtraField] = React.useState(false)
+    const [fieldOfActivityInputValue, setFieldOfActivityInputValue] = React.useState('initial')
 
     const rePhoneNumber = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
-    Yup.addMethod(Yup.string, "phone", function() {
-        return this.test("phone", "Некорректный номер", value =>
+    Yup.addMethod(Yup.string, "phone", function () {
+        return this.test("phone", "Некорректный номер телефона", value =>
             rePhoneNumber.test(value)
         );
     });
@@ -72,41 +83,53 @@ const WorkshopForm = ({type, }) => {
     const Schema = Yup.object().shape({
         name: Yup.string()
             .min(2, 'Слишком коротко')
-            .max(50, 'Слишком длинные ФИО')
+            .max(50, 'Слишком длинное имя')
             .required('Пожалуйста, введите имя'),
         phone: Yup.string()
-            .min(11, 'Пожалуйста, в формате +7xxxxxxxxxx')
-            .max(16, 'Слишком длинный номер телефона')
-            .phone()
-            .required('Пожалуйста, введите номер телефона'),
-        email: Yup.string()
-            .email('E-mail введен некорректно')
-            .required('Пожалуйста, введите почту'),
-        message: Yup.string()
+            .min(18, 'Номер введен неверно')
+            .max(18, 'Слишком длинный номер телефона 😢')
+            // .matches('^([A-Z]+, )?$', 'Пожалуйста, только цифры')
+            // .number()
+            // .phone()
+            .required('Это поле тоже обязательное'),
+        weddingDate: Yup.string()
+            .required('Пожалуйста, введите дату')
+            .min(10, 'Введите дату в формате ДД.ММ.ГГГГ')
+            .max(10, 'Введите дату в формате ДД.ММ.ГГГГ'),
+        city: Yup.string()
+            .required('Пожалуйста, укажите, откуда Вы'),
+        guestsAmount: Yup.string()
+            .required('Пожалуйста, укажите количество гостей')
             .min(2, 'Слишком мало букв 😢')
-            .max(3000, 'Слишком много букв 😢')
-            .required('Пожалуйста, введите текст сообщения'),
-        juridical: Yup.string()
-            .max(50, 'Слишком длинное название')
-            .required('Пожалуйста, введите юридическое название'),
-        commercial: Yup.string()
-            .max(50, 'Слишком длинное название')
-            .required('Пожалуйста, введите коммерческое название'),
-        website: Yup.string()
-            .min(3, 'Слишком короткая ссылка')
-            .max(300, 'Слишком длинная ссылка')
+            .max(15, 'Слишком много букв 😢'),
+        requiredSpecialist: Yup.string()
+            .required('Пожалуйста, укажите специалиста'),
+        companyName: Yup.string()
+            .required('Пожалуйста, укажите название компании')
+            .min(2, 'Слишком мало букв 😢')
+            .max(20, 'Слишком много букв 😢'),
+        fieldOfActivity: Yup.string()
+            .required('Пожалуйста, укажите сферу своей деятельности'),
+        participationVariant: Yup.string()
+            .required('Пожалуйста, укажите формат участия'),
+        link: Yup.string()
+            .required('Пожалуйста, укажите ссылку (или напишите "нет")')
+
+
     });
 
     const data = {};
 
     const onSubmit = (values, {resetForm}) => {
+
+        console.log(values)
         data.form = {
-            "url": `https://partners.rivierasunrise.ru/partners`
+            "url": `https://workshop.rivierasunrise.ru/`
         };
         data.inputs = [
             {
                 "alias": "Источник",
-                "value": "Форма со страницы Партнёрам",
+                "value": "Форма со страницы Свадебного Воркшопа",
             },
             {
                 "alias": "Имя",
@@ -166,32 +189,36 @@ const WorkshopForm = ({type, }) => {
 
     };
 
+
     const handleFieldOfActivityChange = (e) => {
-        console.log(e)
+        setFieldOfActivityInputValue(e.target.value)
+        if (e.target.value === 'other') setShowExtraField(true)
+        else setShowExtraField(false)
     }
+
 
     if (type === "bride") return <Formik
         initialValues={{
-
+            requiredSpecialist: 'initial'
         }}
         validationSchema={Schema}
         onSubmit={onSubmit}>
         <Form>
             <Field component={Input} name="name" placeholder={'Имя'}/>
             <Field component={NumInput} name="phone" placeholder={'Номер телефона'}/>
-            <Field component={Input} name="weddingDate" placeholder={'Дата свадьбы'}/>
+            <Field component={DateInput} name="weddingDate" placeholder={'Дата свадьбы'}/>
             <Field component={Input} name="city" placeholder={'Город'}/>
-            <Field component={Input} name="questsAmount" placeholder={'Количество гостей на свадьбе'}/>
+            <Field component={Input} name="guestsAmount" placeholder={'Количество гостей на свадьбе'}/>
             <Field as="select" name="requiredSpecialist">
-                <option value="disabled" disabled>Какого специалиста Вы ищите?</option>
-                <option value="place">Площадка</option>
-                <option value="host">Ведущий</option>
-                <option value="decorator">Декоратор</option>
-                <option value="stylist">Стилист</option>
-                <option value="photograph">Фотограф</option>
-                <option value="videograph">Видеограф</option>
-                <option value="organizator">Организатор</option>
-                <option value="conditer">Кондитер</option>
+                <option value="initial" disabled>Какого специалиста Вы ищите?</option>
+                <option value="place">Нужна площадка</option>
+                <option value="host">Нужен ведущий</option>
+                <option value="decorator">Нужен декоратор</option>
+                <option value="stylist">Нужен стилист</option>
+                <option value="photograph">Нужен фотограф</option>
+                <option value="videograph">Нужен видеограф</option>
+                <option value="organizator">Нужен организатор</option>
+                <option value="conditer">Нужен кондитер</option>
             </Field>
 
             <button style={{width: '100%'}} className={s.button} type="submit">Зарегистрироваться</button>
@@ -200,7 +227,8 @@ const WorkshopForm = ({type, }) => {
 
     else if (type === "organizator") return <Formik
         initialValues={{
-
+            fieldOfActivity: 'initial',
+            participationVariant: 'initial'
         }}
         validationSchema={Schema}
         onSubmit={onSubmit}>
@@ -208,27 +236,28 @@ const WorkshopForm = ({type, }) => {
             <Field component={Input} name="name" placeholder={'Имя'}/>
             <Field component={NumInput} name="phone" placeholder={'Номер телефона'}/>
             <Field component={Input} name="companyName" placeholder={'Название компании'}/>
-            <Field as="select" name="fieldOfActivity" onChange={handleFieldOfActivityChange}>
-                <option value="disabled" disabled>Сфера деятельности</option>
-                <option value="place">Площадка</option>
-                <option value="host">Ведущий</option>
-                <option value="decorator">Декоратор</option>
-                <option value="stylist">Стилист</option>
-                <option value="photograph">Фотограф</option>
-                <option value="videograph">Видеограф</option>
-                <option value="organizator">Организатор</option>
-                <option value="conditer">Кондитер</option>
+            <Field as="select" name="fieldOfActivity" value={fieldOfActivityInputValue}
+                   onChange={handleFieldOfActivityChange}>
+                <option value="initial" disabled>Сфера деятельности</option>
+                <option value="place">Владелец площадки</option>
+                <option value="host">Я ведущий</option>
+                <option value="decorator">Я декоратор</option>
+                <option value="stylist">Я стилист</option>
+                <option value="photograph">Я фотограф</option>
+                <option value="videograph">Я видеограф</option>
+                <option value="organizator">Я организатор</option>
+                <option value="conditer">Я кондитер</option>
                 <option value="other">Свой вариант</option>
             </Field>
-            {true && <Field component={Input} name="otherActivity" placeholder={'Сфера деятельности'}/>}
+            {showExtraField && <Field component={Input} name="otherActivity" placeholder={'Сфера деятельности'}/>}
             <Field as="select" name="participationVariant">
-                <option value="red" disabled>Вариант участия в мероприятии</option>
+                <option value="initial" disabled>Вариант участия в мероприятии</option>
                 <option value="guest">Гость</option>
-                <option value="Coorganizator">Соорганизатор</option>
+                <option value="coorganizator">Соорганизатор</option>
             </Field>
             <Field component={Input} name="link" placeholder={'Ссылка на сайт/соцсеть'}/>
 
-            <button style={{width: '100%'}} className={s.button} type="submit">Отправить заявку на сотрудничество</button>
+            <button style={{width: '100%'}} className={s.button} type="submit">Зарегистрироваться</button>
         </Form>
     </Formik>
 
